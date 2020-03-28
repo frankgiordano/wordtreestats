33762ffc3ec9616c3ef0f13de5eb5b25a74f637d
Frank Giordano <giofr01@yahoo.com>
2020-03-27 21:19:13 -0400

    remove VS specific stuff no longer needed as using cLion IDE for now

--- WordTreeStats/RBTree.cpp
+++ WordTreeStats/RBTree.cpp
@@ -1,4 +1,3 @@
-#include "pch.h"
 #include "RBTree.h"
 
 RBTree::RBTree() {
--- WordTreeStats/WordTreeStats.cpp
+++ WordTreeStats/WordTreeStats.cpp
@@ -1,5 +1,4 @@
 // WordTreeStats.cpp : This file contains the 'main' function. Program execution begins and ends there.
-#include "pch.h"
 #include <iostream>
 #include <fstream>
 #include "RBTree.h"
